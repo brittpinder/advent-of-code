9a25d6a1b08a524c9165b2da776fdfcd07f291c0
Brittany Pinder <brittpinder@gmail.com>
2022-12-19 21:32:46 -0400

    Day 18, 2022 (Part 1): Boiling Boulders

---
+++ 2022/18/01.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+struct Position {
+    var x: Int
+    var y: Int
+    var z: Int
+
+    static func == (left: Position, right: Position) -> Bool {
+        return left.x == right.x && left.y == right.y && left.z == right.z
+    }
+
+    func getAdjacentPositions() -> [Position] {
+        return [Position(x: self.x + 1, y: self.y, z: self.z),
+                Position(x: self.x - 1, y: self.y, z: self.z),
+                Position(x: self.x, y: self.y + 1, z: self.z),
+                Position(x: self.x, y: self.y - 1, z: self.z),
+                Position(x: self.x, y: self.y, z: self.z + 1),
+                Position(x: self.x, y: self.y, z: self.z - 1)]
+    }
+}
+
+let coordinates = try! String(contentsOfFile: "input")
+                        .split(separator: "\n")
+                        .map{$0.split(separator: ",")}
+                        .map{Position(x: Int($0[0])!, y: Int($0[1])!, z: Int($0[2])!)}
+
+var surfaceArea = 6 * coordinates.count
+
+for coordinate in coordinates {
+    for position in coordinate.getAdjacentPositions() {
+        if coordinates.contains(where: {$0 == position}) {
+            surfaceArea -= 1
+        }
+    }
+}
+
+print(surfaceArea)
+assert(surfaceArea == 4548, "Wrong answer! Expected 4548")
